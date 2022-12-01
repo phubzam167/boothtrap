8c941bf9efe2b2f827143767330c2310ff50e455
Phub Zam <you@example.com>
2022-12-01 14:23:41 +0600

    boothstrap changes

--- script.js
+++ script.js
@@ -8,3 +8,4 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
